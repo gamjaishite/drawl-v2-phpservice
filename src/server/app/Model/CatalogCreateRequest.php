f5d90af999f81b8aa6c59f05f4c2003b49391797
debbyalmadea <almadeaputri@gmail.com>
2023-09-25 23:42:39 +0700

    add: Catalog Controller, Model, and Service

---
+++ src/server/app/Model/CatalogCreateRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+class CatalogCreateRequest
+{
+    public ?string $title = null;
+    public ?string $description = null;
+    public $poster = null;
+    public ?string $trailer = null;
+    public ?string $category = null;
+}
