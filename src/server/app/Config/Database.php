84b6df55445c3c6eb463c3d51ba07cf1d66d7c6b
debbyalmadea <almadeaputri@gmail.com>
2023-09-27 20:54:00 +0700

    add: tags schema

--- src/server/app/Config/Database.php
+++ src/server/app/Config/Database.php
@@ -7,29 +7,14 @@ class Database
     public static function getConnection(string $env = 'dev'): \PDO
     {
         if (self::$pdo == null) {
-            try {
-                // create new POD
-                require_once __DIR__ . '/../../config/database.php';
-                $config = getDatabaseConfig();
-                self::$pdo = new \PDO(
-                    $config['database'][$env]['url'],
-                    $config['database'][$env]['username'],
-                    $config['database'][$env]['password']
-                );
-            } catch (\PDOException $e) {
-                // if ($e->getCode() == 7 && $env == 'dev') {
-                //     self::$pdo = new \PDO(
-                //         $config['database'][$env]['host_url'],
-                //         $config['database'][$env]['username'],
-                //         $config['database'][$env]['password']
-                //     );
-
-                //     $statement = self::$pdo->prepare("CREATE DATABASE " . $config['database'][$env]['name']);
-                //     $statement->execute();
-                // } else {
-                throw $e;
-                // }
-            }
+            // create new POD
+            require_once __DIR__ . '/../../config/database.php';
+            $config = getDatabaseConfig();
+            self::$pdo = new \PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password']
+            );
         }
         return self::$pdo;
     }
