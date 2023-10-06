7ed4a288c0584f2ac03c5a0048f654986d7d9c5b
debbyalmadea <almadeaputri@gmail.com>
2023-10-06 15:37:36 +0700

    add: profile menu on navbar

--- src/public/js/components/navbar.js
+++ src/public/js/components/navbar.js
@@ -24,3 +24,33 @@ if (navbarToggle) {
     }
   });
 }
+
+profileMenuToggle = document.getElementById("profile-menu-toggle");
+if (profileMenuToggle) {
+  profileMenuToggle.addEventListener("click", function () {
+    profileMenu = document.getElementById("profile-menu");
+    if (profileMenu.classList.contains("collapsed")) {
+      profileMenu.classList.remove("collapsed");
+      profileMenuToggle.focus();
+      this.setAttribute("aria-expanded", "true");
+    } else {
+      profileMenu.classList.add("collapsed");
+      profileMenuToggle.blur();
+      this.setAttribute("aria-expanded", "false");
+    }
+  });
+
+  profileMenuToggle.addEventListener("blur", function (e) {
+    if (
+      e.relatedTarget &&
+      e.relatedTarget.parentElement.id === "profile-menu"
+    ) {
+      return;
+    } else {
+      profileMenu = document.getElementById("profile-menu");
+      profileMenu.classList.add("collapsed");
+      profileMenuToggle.blur();
+      this.setAttribute("aria-expanded", "false");
+    }
+  });
+}
--- src/server/app/View/components/navbar.php
+++ src/server/app/View/components/navbar.php
@@ -5,18 +5,25 @@
                 <a href='/' class="brand__title">Drawl</a>
             </div>
             <button aria-label="Open Menu" id="navbar-toggle" class="navbar-toggle" aria-expanded="false"
-                    aria-controls="navbar-menu">
+                aria-controls="navbar-menu">
                 <?php require PUBLIC_PATH . 'assets/icons/menu.php' ?>
             </button>
         </div>
         <div id="navbar-menu" class="navbar-menu collapsed" aria-labelledby="navbar-toggle">
             <a href="/" class="btn">Discover</a>
             <a href="/catalog" class="btn">Catalog</a>
-            <a href="/profile/watchlist" class="btn">My Watch List</a>
             <?php if ($user == null): ?>
                 <a href="/signin" class="btn">Sign In</a>
             <?php else: ?>
-                <a href="/profile" class="btn"><?= $user->name ?></a>
+                <button id="profile-menu-toggle" class="profile-icon" aria-label="Open Profile Dropdown"
+                    aria-expanded="false">
+                    <?php require PUBLIC_PATH . 'assets/icons/user.php' ?>
+                </button>
+                <div id="profile-menu" class="profile-menu collapsed" aria-labelledby="profile-menu-toggle">
+                    <a href="/profile" class="btn">Profile</a>
+                    <a href="/profile/watchlist" class="btn">My Watchlist</a>
+                    <a href="/profile/bookmark" class="btn">My Bookmark</a>
+                </div>
             <?php endif; ?>
         </div>
     </div>
