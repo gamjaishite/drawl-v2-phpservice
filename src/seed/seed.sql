0a13ffc81ec6d11f4b75de8df781c28c07061333
debbyalmadea <almadeaputri@gmail.com>
2023-10-08 19:51:18 +0700

    fix: fixed position on mobile in create/update watchlists

--- src/seed/seed.sql
+++ src/seed/seed.sql
@@ -1,21 +1,19 @@
--- Seed data for 'ANIME' category
 INSERT INTO catalogs (uuid, title, description, poster, trailer, category)
 SELECT
     md5(random()::text || clock_timestamp()::text)::uuid,
-    'Anime Title ' || generate_series(1, 10),
-    'Anime Description ' || generate_series(1, 10),
+    'Anime Title ' || num,
+    'Anime Description ' || num,
     '0f57456ef87ea61a.webp',
     '86fa25a6dad7fcc7.mp4',
     'ANIME'
-FROM generate_series(1, 10);
+FROM generate_series(1, 100) AS num;
 
--- Seed data for 'DRAMA' category
 INSERT INTO catalogs (uuid, title, description, poster, trailer, category)
 SELECT
     md5(random()::text || clock_timestamp()::text)::uuid,
-    'Drama Title ' || generate_series(1, 10),
-    'Drama Description ' || generate_series(1, 10),
+    'Drama Title ' || num,
+    'Drama Description ' || num,
     'a9f6e15daf0eca96.webp',
     '86fa25a6dad7fcc7.mp4',
     'DRAMA'
-FROM generate_series(1, 10);
+FROM generate_series(1, 100) AS num;
